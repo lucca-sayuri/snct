a651d980f7a6cdadc72f759a8071d83abd1bc5cf
lucca <kelcoolg@gmail.com>
2025-10-11 20:04:29 -0300

    arrow enemies working, word enemies need fixing

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const menuButton = document.querySelector("#menuButton")
 const ctx = canvas.getContext('2d')
 const play = document.querySelector("#play")
 const enemies = []
-let spawnInterval = 100
+const chewy = new FontFace('chewy', 'url(assets/Chewy-Regular.ttf)')
+const fira = new FontFace('fira', 'url(assets/FiraCode-SemiBold.ttf')
+const grover = new FontFace('grover', 'url(assets/IrishGrover-Regular.ttf')
+
+chewy.load().then(function (font) {
+    document.fonts.add(font)
+})
+fira.load().then(function (font) {
+    document.fonts.add(font)
+})
+
+let spawnInterval = 2000
 let defeatedEnemies = 0
 let spawnedEnemies = 0
 const maxEnemies = 25
@@ -15,12 +26,8 @@ canvas.height = 720
 play.addEventListener("click", () => {
     game.classList.remove("hidden")
     menu.classList.add("hidden")
-    setTimeout(() => {
-        const enemyInterval = setInterval(spawnEnemy, spawnInterval)
-    }, 2000);
 })
 menuButton.addEventListener("click", () => {
-    clearInterval(enemyInterval)
     enemies.splice(0)
     game.classList.add("hidden")
     menu.classList.remove("hidden")
@@ -103,18 +110,21 @@ class Player {
 
 class Enemy {
     constructor() {
+        this.colors = ["black", "darkslategray", "darkgray", "crimson", "darkblue", "darkcyan", "darkmagenta", "darkorchid", "darkslateblue", "hotpink", "indigo", "midnightblue", "navy", "red", "saddlebrown", "teal", "maroon",  "brown", "sienna"]
+        this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
         this.pos = this.position()
         this.array = this.randomArray()
         this.width = 150
         this.height = 150
         this.fps = 4
-
         if (this.pos === "left") {
             this.x = 0 - this.width
             this.speed = 1
+            this.arrows = this.convertArrows()
         } else {
             this.x = canvas.width
             this.speed = 1.1 - (this.array.length / 10)
+            this.word = this.array.join("")
         }
 
         this.y = (canvas.height * 0.95) - this.height
@@ -134,20 +144,49 @@ class Enemy {
             const arrowArray = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]
             const arrows = []
             let rNumberArrow = 0
-            for (const arrow of arrowArray) {
+            for (let i = 0; i < 4; i++) {
                 rNumberArrow = Math.floor(Math.random() * arrowArray.length)
                 arrows.push(arrowArray[rNumberArrow])
+                arrowArray.splice(rNumberArrow, 1)
             }
             return (arrows)
         } else {
-            const words = ["planta", "folha", "reciclar", "reusar", "repensar", "arvore", "flor", "reduzir", "repassar", "conservar", "ambiente", "eco", "energia", "mudança"]
+            const words = ["planta", "folha", "reciclar", "reusar", "repensar", "flor", "reduzir", "repassar", "conservar", "ambiente", "eco", "energia", "mudança"]
             let rNumberWord = Math.floor(Math.random() * words.length)
             let word = words[rNumberWord]
             let wordArray = word.split("")
             return (wordArray)
         }
     }
+    convertArrows() {
+        let array = []
+        let arrows = ""
+        this.array.forEach((arrow, index) => {
+            switch (true) {
+                case (this.array[index] === "ArrowLeft"):
+                    array.push("←")
+                    break
+
+                case (this.array[index] === "ArrowRight"):
+                    array.push("→")
+                    break
+
+                case (this.array[index] === "ArrowDown"):
+                    array.push("↓")
+                    break
+
+                case (this.array[index] === "ArrowUp"):
+                    array.push("↑")
+                    break
+            }
+            arrows = array.join(" ")
+        });
+        return(arrows)
+    }
     update(deltaTime) {
+        if (this.pos === "left") {
+            this.arrows = this.convertArrows()
+        }
         if (input.keys.includes(this.array[0]) === true) {
             this.array.splice(0, 1)
             console.log(this.array)
@@ -172,7 +211,14 @@ class Enemy {
         }
     }
     draw() {
-        ctx.fillStyle = "red"
+        ctx.fillStyle = this.color
+        ctx.textAlign = "center"
+        if (this.pos === "left") {
+            ctx.font = "bold 48px fira"
+            ctx.fillText(this.arrows, this.x + (this.width / 2), this.y - (this.height * 0.3))
+        } else {
+            ctx.font = "40px grover"
+        }
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
@@ -189,6 +235,8 @@ function spawnEnemy() {
         console.log(enemy.array)
     }
 }
+
+setInterval(spawnEnemy, 3000)
 
 function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime
