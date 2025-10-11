d7dd4f6ba68a3c351636cfdb667971722e19a57d
lucca <kelcoolg@gmail.com>
2025-10-10 22:04:50 -0300

    enemies updated, enemy class changed, menu and game polish

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ const menuButton = document.querySelector("#menuButton")
 const ctx = canvas.getContext('2d')
 const play = document.querySelector("#play")
 const enemies = []
-const defeatedEnemies = 0
-const spawnedEnemies = 0
-const maxEnemies = 10
+let spawnInterval = 100
+let defeatedEnemies = 0
+let spawnedEnemies = 0
+const maxEnemies = 25
 canvas.width = 1480
-canvas.height = 840
+canvas.height = 720
 
 play.addEventListener("click", () => {
     game.classList.remove("hidden")
     menu.classList.add("hidden")
+    setTimeout(() => {
+        const enemyInterval = setInterval(spawnEnemy, spawnInterval)
+    }, 2000);
 })
 menuButton.addEventListener("click", () => {
+    clearInterval(enemyInterval)
+    enemies.splice(0)
     game.classList.add("hidden")
     menu.classList.remove("hidden")
 })
@@ -31,7 +37,6 @@ class InputHandler {
         document.addEventListener('keyup', e => {
             console.log(this.keys)
             this.keys.splice(this.keys.indexOf(e.key), 1)
-            console.log(this.keys)
         })
     }
 }
@@ -41,19 +46,20 @@ const input = new InputHandler
 class Player {
     constructor() {
         this.width = 400,
-        this.height = 400,
-        this.x = canvas.width * 0.1,
-        this.y = canvas.height * 0.5,
+            this.height = 400,
+            this.x = (canvas.width / 2) - (this.width / 2),
+            this.y = (canvas.height * 0.95) - this.height
         this.frameX = 0,
-        this.frameY = 0,
-        this.maxFrameX = 7,
-        this.fps = 4,
-        this.frameInterval = 1000 / 4,
-        this.frameTimer = 0,
-        this.idle = true,
-        this.sprite = document.querySelector("#playerSprite")
+            this.frameY = 0,
+            this.maxFrameX = 7,
+            this.fps = 4,
+            this.frameInterval = 1000 / 4,
+            this.frameTimer = 0,
+            this.idle = true,
+            this.sprite = document.querySelector("#playerSprite")
+        this.hearts = 3
     }
-    attack() {
+    attackRight() {
         this.idle = false
         setTimeout(() => {
             this.frameX = 0
@@ -64,6 +70,20 @@ class Player {
         this.frameX = 0
         this.frameY = 1
         this.maxFrameX = 1
+    }
+    attackLeft() {
+        this.idle = false
+        setTimeout(() => {
+            this.frameX = 0
+            this.frameY = 0
+            this.maxFrameX = 7
+        }, 500);
+        this.frameX = 0
+        this.frameY = 2
+        this.maxFrameX = 1
+    }
+    hurt() {
+        this.hearts--
     }
     update(deltaTime) {
         if (this.frameTimer > this.frameInterval) {
@@ -82,21 +102,50 @@ class Player {
 }
 
 class Enemy {
-    randomWord() {
-        const words = ["planta", "folha", "reciclar", "reusar", "repensar", "arvore", "flor", "reduzir", "repassar", "conservar", "ambiente", "eco", "energia", "mudança"]
-        let rNumber = Math.floor(Math.random() * words.length)
-        return(words[rNumber])
-    }
     constructor() {
-        this.word = this.randomWord()
-        this.array = this.word.split("")
-        this.width = 350
-        this.height = 350
+        this.pos = this.position()
+        this.array = this.randomArray()
+        this.width = 150
+        this.height = 150
         this.fps = 4
-        this.x = canvas.width - this.width
-        this.y = canvas.height * 0.5
-        this.image = document.querySelector("#placeholder")
-        this.speed = 1.25 - (this.array.length / 10)
+
+        if (this.pos === "left") {
+            this.x = 0 - this.width
+            this.speed = 1
+        } else {
+            this.x = canvas.width
+            this.speed = 1.1 - (this.array.length / 10)
+        }
+
+        this.y = (canvas.height * 0.95) - this.height
+    }
+
+    position() {
+        let randomPos = Math.floor(Math.random() * 2)
+        if (randomPos === 0) {
+            return ("left")
+        } else {
+            return ("right")
+        }
+    }
+
+    randomArray() {
+        if (this.pos === "left") {
+            const arrowArray = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]
+            const arrows = []
+            let rNumberArrow = 0
+            for (const arrow of arrowArray) {
+                rNumberArrow = Math.floor(Math.random() * arrowArray.length)
+                arrows.push(arrowArray[rNumberArrow])
+            }
+            return (arrows)
+        } else {
+            const words = ["planta", "folha", "reciclar", "reusar", "repensar", "arvore", "flor", "reduzir", "repassar", "conservar", "ambiente", "eco", "energia", "mudança"]
+            let rNumberWord = Math.floor(Math.random() * words.length)
+            let word = words[rNumberWord]
+            let wordArray = word.split("")
+            return (wordArray)
+        }
     }
     update(deltaTime) {
         if (input.keys.includes(this.array[0]) === true) {
@@ -105,14 +154,26 @@ class Enemy {
         }
         enemies.forEach((enemy, index) => {
             if (enemy.array.length === 0) {
+                defeatedEnemies++
                 enemies.splice(index, 1)
-                player.attack()
+                player.attackRight()
+            }
+            if ((enemy.pos === "left" && enemy.x > (canvas.width / 2) - (player.width / 2)) ||
+                (enemy.pos === "right" && enemy.x < (canvas.width / 2) + (player.width / 4))) {
+                enemies.splice(index, 1)
+                player.hurt()
             }
         });
-        this.x -= this.speed
+
+        if (this.pos === "left") {
+            this.x += this.speed
+        } else {
+            this.x -= this.speed
+        }
     }
     draw() {
-        ctx.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 
@@ -122,11 +183,12 @@ const player = new Player
 
 function spawnEnemy() {
     const enemy = new Enemy
-    enemies.push(enemy)
-    console.log(enemy.array)
+    if (spawnedEnemies < maxEnemies) {
+        spawnedEnemies++
+        enemies.push(enemy)
+        console.log(enemy.array)
+    }
 }
-
-setInterval(spawnEnemy, 4000)
 
 function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime
